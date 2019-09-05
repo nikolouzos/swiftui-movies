c2615830c3b5756721f2540b2caecba5cd24766d
Nikolaos Rafail Nikolouzos <nickrnikolouzos@gmail.com>
2019-09-05 21:32:00 +0300

    Initial Commit

---
+++ movies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  movies
+//
+//  Created by Nikolaos Rafail Nikolouzos on 5/9/19.
+//  Copyright © 2019 Nikolaos Rafail Nikolouzos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
