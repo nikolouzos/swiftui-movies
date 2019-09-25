038894df1febd3ad345aa8e54cdf34daab248b6d
Nikolaos Rafail Nikolouzos <nickrnikolouzos@gmail.com>
2019-09-25 20:35:29 +0300

    Added the MovieCardView

---
+++ movies/Views/Movies/MovieCardView.swift
@@ -0,0 +1,22 @@
+//
+//  MovieCardView.swift
+//  movies
+//
+//  Created by Nikolaos Rafail Nikolouzos on 25/9/19.
+//  Copyright © 2019 Nikolaos Rafail Nikolouzos. All rights reserved.
+//
+
+import SwiftUI
+
+struct MovieCardView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+struct MovieCardViewPreviews: PreviewProvider {
+    static var previews: some View {
+        MovieCardView()
+            .previewLayout(.sizeThatFits)
+    }
+}
