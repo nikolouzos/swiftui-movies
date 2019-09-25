9bbf105bad56205bad50a50b2cf8b48ab5d26ad5
Nikolaos Rafail Nikolouzos <nickrnikolouzos@gmail.com>
2019-09-25 20:29:41 +0300

    Fixes to the Color extension

--- movies/Models/Extensions/Color.swift
+++ movies/Models/Extensions/Color.swift
@@ -8,27 +8,26 @@
 
 import SwiftUI
 
-
 extension Color {
     init?(hexString: String) {
         var chars = Array(hexString.hasPrefix("#") ? hexString.dropFirst() : hexString[...])
-        let r, g, b, a: CGFloat
+        let red, green, blue, alpha: CGFloat
         switch chars.count {
         case 3:
             chars = chars.flatMap { [$0, $0] }
             fallthrough
         case 6:
-            chars = ["F","F"] + chars
+            chars = ["F", "F"] + chars
             fallthrough
         case 8:
-            a = CGFloat(strtoul(String(chars[0...1]), nil, 16)) / 255
-            r = CGFloat(strtoul(String(chars[2...3]), nil, 16)) / 255
-            g = CGFloat(strtoul(String(chars[4...5]), nil, 16)) / 255
-            b = CGFloat(strtoul(String(chars[6...7]), nil, 16)) / 255
+            alpha = CGFloat(strtoul(String(chars[0...1]), nil, 16)) / 255
+            red = CGFloat(strtoul(String(chars[2...3]), nil, 16)) / 255
+            green = CGFloat(strtoul(String(chars[4...5]), nil, 16)) / 255
+            blue = CGFloat(strtoul(String(chars[6...7]), nil, 16)) / 255
         default:
             return nil
         }
         
-        self.init(.sRGB, red: Double(r), green: Double(g), blue: Double(b), opacity: Double(a))
+        self.init(.sRGB, red: Double(red), green: Double(green), blue: Double(blue), opacity: Double(alpha))
     }
 }
